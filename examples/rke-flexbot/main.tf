9935aa88c5b616c5d122ee4170ddefb9bb2f619a
Igor Feoktistov <igorf@netapp.com>
2020-09-17 15:24:21 -0700

    Initial commit

--- examples/rke-flexbot/main.tf
+++ examples/rke-flexbot/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     flexbot = {
       source  = "igor-feoktistov/flexbot"
-      version = "1.3.2"
+      version = "1.3.3"
     }
     rke = {
       source = "rancher/rke"
